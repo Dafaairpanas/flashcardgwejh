15dfb3e19765e97d632d018ac897c43f35db5951
Dafaairpanasx <dafaoffic@gmail.com>
2026-08-23 14:36:45 +0700

    performance

--- app/layout.jsx
+++ app/layout.jsx
@@ -34,8 +34,6 @@ export default function RootLayout({ children }) {
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Klee+One&display=swap" />
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Noto+Serif+JP:wght@400;700&display=swap" />
         <script
           dangerouslySetInnerHTML={{
             __html: `
